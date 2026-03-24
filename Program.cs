f76bc1ab76e6ea08f8222bfea6e4c34fd3f204b6
Devendra Mahore <devendra.mahore21@gmail.com>
2026-03-24 15:02:21 +0530

    Add Home page as startup page

--- Program.cs
+++ Program.cs
@@ -48,6 +48,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Employee}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
